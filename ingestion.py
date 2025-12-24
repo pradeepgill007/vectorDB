130c8b4bb5031fe33f9f898248af5754dbc26543
pradeepgill007 <pradeepgill713@gmail.com>
2025-12-24 14:29:11 +0100

    save in vector DB

--- ingestion.py
+++ ingestion.py
@@ -24,27 +24,72 @@ load_dotenv()
     ) """
 embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
-#chromeLocalDB = Chroma(persist_directory='chroma_db', embedding_function=embedding)
-vectorStore = PineconeVectorStore(index_name='langchain-doc-index', embedding=embeddings)
+#vectorstore = Chroma(persist_directory='chroma_db', embedding_function=embedding)
+vectorStore = PineconeVectorStore(index_name='langchain-collection-768', embedding=embeddings)
 
 tavilyExtract = TavilyExtract()
 tavilyMap = TavilyMap(max_depth=1,max_breadth=1,max_pages=1)
 tavilyCrawl = TavilyCrawl()
 
+
+async def index_doc_async(documents: List[Document], batch_size:int = 50):
+    """Process documents in batches asynchronously."""
+    log_header("VECTOR STORAGE PHASE")
+    log_info(f"📚 VectorStore Indexing: Preparing to add {len(documents)} documents to vector store", Colors.DARKCYAN)
+    
+    batches = [documents[i : i + batch_size] for i in range(0, len(documents), batch_size)]
+
+    log_info(f"📦 VectorStore Indexing: Split into {len(batches)} batches of {batch_size} documents each")
+
+    async def add_batch(batch: List[Document], batch_num: int):
+        try:
+            await vectorStore.aadd_documents(batch)
+            log_success(f"VectorStore Indexing: Successfully added batch {batch_num}/{len(batches)} ({len(batch)} documents)")
+        except Exception as e:
+            log_error(f"VectorStore Indexing: Failed to add batch {batch_num} - {e}")
+            return False
+        return True
+
+    tasks = [add_batch(batch, i + 1) for i, batch in enumerate(batches)]
+    results = await asyncio.gather(*tasks, return_exceptions=True)
+
+    successful = sum(1 for result in results if result is True)
+
+    if successful == len(batches):
+        log_success(f"VectorStore Indexing: All batches processed successfully! ({successful}/{len(batches)})")
+    else:
+        log_warning(f"VectorStore Indexing: Processed {successful}/{len(batches)} batches successfully")
+
+
+
 async def main():
     log_header('Doc injection pipeline!')
-
     log_info('Web crawler started', Colors.PURPLE)
 
     res = tavilyCrawl.invoke({
         'url':'https://en.wikipedia.org/wiki/Narendra_Modi',
-        'max_depth':1,
+        'max_depth':2,
         'instructions':'content on Narendra Modi'
     })
 
     all_docs = res['results']
-    print(all_docs)
+    print("all_docs ==>", all_docs)
     log_success(f'Finish crawling {len(all_docs)} URLs from the site')
+
+    docs_to_split = []
+    for item in all_docs:
+        doc = Document(
+            page_content=item.get('raw_content') or item.get('content') or '',
+            metadata={'source': item.get('url'), 'title':'Narendra Modi wiki data bete'}
+        )
+        docs_to_split.append(doc)
+
+    text_splitter = RecursiveCharacterTextSplitter()
+    splitted_doc = text_splitter.split_documents(docs_to_split)
+
+    log_success(f'{len(splitted_doc)} chunks created')
+
+    await index_doc_async(splitted_doc, batch_size=20)
 
 if __name__ == "__main__":
     asyncio.run(main())
